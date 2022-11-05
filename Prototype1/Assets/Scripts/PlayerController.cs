07847fd8b5b0207b57d0c9a097337667dbac3708
Ronan Alexis T. Valle <ronanalexistvalle@iskolarngbayan.pup.edu.ph>
2022-11-06 03:42:26 +0800

    Initial Commit
    .gitignore
    .gitattributes

---
+++ Prototype1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Variables
+    public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Player Input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //Moving the Vehicle Forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //Turn the Vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
